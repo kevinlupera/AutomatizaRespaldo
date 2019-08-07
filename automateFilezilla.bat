ce8ea2e4a50977f0c7b212a146740e614eea150d
Kevin Lupera <klupera@uees.edu.ec>
2019-08-07 17:29:10 -0500

    Primer commit

---
+++ automateFilezilla.bat
@@ -0,0 +1,27 @@
+@echo on
+:: Crea carpeta yyyyMMdd
+for /F "tokens=1-3 delims=/ " %%A in ('date/t') do (
+	set DateDay=%%B
+	set DateMonth=%%A
+	set DateYear=%%C
+)
+SET ruta=%~dp0
+set CurrentDate=%DateMonth%%DateDay%%DateYear%
+mkdir %ruta%\Respaldos_automatizados\%CurrentDate%
+::Ruta del WinSCP
+"C:\Program Files (x86)\WinSCP\WinSCP.com" ^
+  /log="%ruta%\Respaldos_automatizados\log\WinSCP.log" /ini=nul ^
+  /command ^
+    "open {sftp://host/del/servidor} -privatekey=""%ruta%\clave_privada.ppk"" -rawsettings Utf=1 ProxyPort=1" ^
+	"stat {/ruta/de/el/proyecto}" ^
+    "get {/ruta/de/el/respaldo/zip/ecommerce.zip} {%ruta%\Respaldos_automatizados\%CurrentDate%\}"  ^
+    "exit"
+
+set WINSCP_RESULT=%ERRORLEVEL%
+if %WINSCP_RESULT% equ 0 (
+  echo Success
+) else (
+  echo Error
+)
+
+echo %WINSCP_RESULT%
---
+++ respaldo.bat
@@ -0,0 +1,17 @@
+@echo on
+:: Crea carpeta yyyyMMdd
+for /F "tokens=1-4 delims=/ " %%A in ('date/t') do (
+	set DateDay=%%B
+	set DateMonth=%%A
+	set DateYear=%%C
+)
+SET ruta=%~dp0
+set CurrentDate=%DateMonth%%DateDay%%DateYear%
+mkdir %ruta%\Respaldos_automatizados
+mkdir %ruta%\Respaldos_automatizados\%CurrentDate%
+::Abre putty y carga la sesión
+>%ruta%\Respaldos_automatizados\%CurrentDate%\log%CurrentDate%.log (plink {nombre de la sesion de putty} -m %ruta%\commands.txt)
+>%ruta%\Respaldos_automatizados\%CurrentDate%\logFilezilla%CurrentDate%.log (call %ruta%\automateFilezilla.bat)
+echo Respaldo %CurrentDate% ejecutado exitosamente! 
+echo Confirmar el respaldo.
+&pause
